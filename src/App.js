5ac788fa2a641d3107ac53343f4615afae66627e
Danielle Koski <dkoski4@yahoo.com>
2021-01-14 15:56:43 -0800

    tested sticky header and updated App.js with imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import FormRequest from "./components/FormRequest/FormRequest";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main>Placeholder</main>
+      <FormRequest />
       <Footer />
     </div>
   );
---
+++ src/components/FormRequest/FormRequest.js
@@ -0,0 +1,14 @@
+import "./FormRequest.css";
+
+function FormRequest(props) {
+  return (
+    <main>
+      <header>
+        <h1>Contact Us</h1>
+        <div></div>
+      </header>
+    </main>
+  );
+}
+
+export default FormRequest;
